6c84779765db9e6a5e5d746ab5bddbc151ef8f63
anjiuidev <anjisingavaram@gmail.com>
2018-04-29 16:46:51 +0530

    Basic functionality which will creates 8X8 boxes dynamically and added the click function, if the diamond exists it will show else arrow will show. If all the diamonds are visible game is over and remaining unclicked boxes is the score

--- diamond-sweeper/app/client/app.js
+++ diamond-sweeper/app/client/app.js
@@ -1,3 +1,52 @@
-global.startApp = function(container) {
-  console.log("Here is the container:", container);
+global.startApp = function (container) {
+  //  console.log("Here is the container:", container);
+  var table = document.querySelector('.diamondsweeper-board');
+  var rows = 8,
+      cols = 8,
+      index = 0,
+      score = 0,
+      diamondsFound = 0,
+      remainingBoxes = 0;
+  for (var i = 0; i < rows; i++) {
+      var tr = document.createElement('tr');
+      table.appendChild(tr);
+      for (var j = 0; j < cols; j++) {
+          var td = document.createElement('td');
+          td.setAttribute('id', index);
+          td.setAttribute('class', 'cell unknown');
+          tr.appendChild(td);
+          index++;
+          td.addEventListener('click', boxClick);
+      }
+  }
+  var td = document.querySelectorAll('.diamondsweeper-board tr td');
+  var totalBoxes = td.length;
+  var diamondIndexes = [];
+  while (diamondIndexes.length < 8) {
+      var diamondIndex = Math.floor(Math.random() * totalBoxes);
+      if (diamondIndexes.indexOf(diamondIndex) == -1) {
+          diamondIndexes.push(diamondIndex);
+      }
+  }
+  console.log(diamondIndexes)
+
+  function boxClick() {
+      var boxIndex = parseInt(this.getAttribute('id'));
+      if (this.classList.contains('unknown')) {
+          this.classList.remove('unknown');
+          score += 1;
+          if (diamondIndexes.indexOf(boxIndex) == -1) {
+              this.classList.add('arrow');
+          } else {
+              this.classList.add('diamond');
+              diamondsFound += 1;
+              remainingBoxes = totalBoxes - score;
+              if (diamondsFound >= 8) {
+                  setTimeout(function () {
+                      alert('Game is Over! Your Score is ' + remainingBoxes);
+                  }, 300)
+              }
+          }
+      }
+  }
 }
